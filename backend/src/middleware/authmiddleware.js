16253cf542c8a17d4f6e0e568b5c110c68f2c22f
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-05 19:27:20 +0500

    created auth middleware check

---
+++ backend/src/middleware/authmiddleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+
+const isAuthenticated = async (
+    req,
+    res,
+    next
+) => {
+    try {
+        console.log('JWT in session:', req.session?.jwt);
+
+        
+        if (!req.session?.jwt) {
+            return res.status(409).json({
+                success: false,
+                message: 'Unauthorized Access'
+            });
+        }
+
+        const decodedData = jwt.verify(
+            req.session.jwt,
+            process.env.JWT_KEY
+        );
+
+        req.currentUser = decodedData;
+        next();
+    } catch (error) {
+        console.error('Error in isAuthenticated middleware:', error);
+        res.status(401).json({ error: "Unauthorized" });
+    }
+};
+
+export { isAuthenticated };
