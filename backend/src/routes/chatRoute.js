a3822299c15b3b1feb85e5ac421249d283f2c806
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-07 00:57:35 +0500

    group renma requires a put request.

--- backend/src/routes/chatRoute.js
+++ backend/src/routes/chatRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/api/chat', isAuthenticated, accessChat);
 router.get('/api/chat/all', isAuthenticated, allUserChats);
 router.post('/api/chat/group', isAuthenticated, createGroupChat);
-router.post('/api/chat/group/rename', isAuthenticated, renameGroupName);
+router.put('/api/chat/group/rename', isAuthenticated, renameGroupName);
 router.post('/api/chat/group/add', isAuthenticated, addUsersToGroup);
 router.delete('/api/chat/group/remove', isAuthenticated, removeUsersFromGroup);
 
