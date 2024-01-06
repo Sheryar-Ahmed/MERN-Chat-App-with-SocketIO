08fe4a8fabd355a4f4b6cfc3213c99782f189485
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-07 00:34:37 +0500

    done creation of a group chat.

--- backend/src/controller/chat.js
+++ backend/src/controller/chat.js
@@ -1,4 +1,8 @@
 const Chat = require('../models/chat');
+const User = require('../models/auth');
+
+
+
 
 const accessChat = async (req, res) => {
     const { userId } = req.body;
@@ -51,10 +55,19 @@ const accessChat = async (req, res) => {
 const allUserChats = async (req, res) => {
     try {
         const userId = req.currentUser.id;
-        const allChats = await Chat.find({ 'users': userId });
+        const allChats = await Chat.find({ 'users': userId })
+            .populate("users")
+            .populate("groupAdmin")
+            .populate("latestMessage")
+            .sort({ updatedAt: -1 })
+            ;
+        const withLatestMessage = await User.populate(allChats, {
+            path: "latestMessage.sender",
+            select: "username email"
+        })
         return res.status(200).json({
             success: true,
-            userChats: allChats,
+            userChats: withLatestMessage,
         })
     } catch (error) {
         console.log("Error occured during fetching of userchats", error);
@@ -65,4 +78,58 @@ const allUserChats = async (req, res) => {
     }
 }
 
-module.exports = { accessChat, allUserChats };
+
+
+const createGroupChat = async (req, res) => {
+    if (!req.body.users || !req.body.groupName) {
+        return res.status(400).json({
+            message: "Please Fill out all the required fields first"
+        })
+    };
+
+    const users = req.body.users;
+
+    if (users.length < 2) {
+        return res.status(400).json({
+            message: "More than 2 users are can create a group"
+        })
+    };
+    //current user is also the part of the group.
+    users.push(req.currentUser.id);
+
+    try {
+        const chat = await Chat.create({
+            chatName: req.body.groupName,
+            users: users,
+            isGroupChat: true,
+            groupAdmin: req.currentUser.id
+        });
+
+        const FullChat = await Chat.findOne({
+            _id: chat._id
+        }).populate({
+            path: "groupAdmin",
+            model: "Users",
+        })
+        .populate({
+            path: "users",
+            match: { _id: { $ne: req.currentUser.id } }, // Exclude current user
+            model: "Users", 
+        });
+        return res.status(201).json({
+            success: true,
+            groupChat: FullChat
+        })
+    } catch (error) {
+        console.log("Error occured during creation of a group chat", error);
+        return res.status(500).json({
+            success: false,
+            message: "Something bad happened"
+        });
+    }
+
+}
+
+
+
+module.exports = { accessChat, allUserChats, createGroupChat };
--- backend/src/routes/chatRoute.js
+++ backend/src/routes/chatRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { accessChat, allUserChats } = require('../controller/chat');
+const { accessChat, allUserChats, createGroupChat } = require('../controller/chat');
 const { isAuthenticated } = require('../middleware/authmiddleware');
 const router = express.Router();
 
 router.post('/api/chat', isAuthenticated, accessChat);
 router.get('/api/chat/all', isAuthenticated, allUserChats);
+router.post('/api/chat/group', isAuthenticated, createGroupChat);
 
 module.exports = router;
