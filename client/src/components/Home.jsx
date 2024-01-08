11a678da71190cf70dd9a1e417533da8d405c87d
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-08 23:01:35 +0500

    created header for chats

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './Header';
 
 const Home = () => {
   
   return (
     <div>
+      <Header />
         Show if the user is logged in his chats else take him to the auth page.
     </div>
   )
