678cc1315928f418184fe7cc61163188f86040af
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-11 23:10:10 +0500

    user search action is ready

--- client/src/state/actions/chatActions.js
+++ client/src/state/actions/chatActions.js
@@ -1,5 +1,5 @@
-import { CHATS_FAIL, CHATS_REQUEST, CHATS_SUCCESS } from '../constants/chatConstant';
-import { chatUrl } from '../../utils/requestUrls';
+import { CHATS_FAIL, CHATS_REQUEST, CHATS_SUCCESS, USER_CHATS_SEARCH_FAIL, USER_CHATS_SEARCH_REQUEST, USER_CHATS_SEARCH_SUCCESS } from '../constants/chatConstant';
+import { chatSearchUserUrl, chatUrl } from '../../utils/requestUrls';
 import buildClient from '../../utils/requestUrls';
 
 export const userChatList = ({ email, password, onSuccess, onFail }) => async (dispatch) => {
@@ -29,6 +29,45 @@ export const userChatList = ({ email, password, onSuccess, onFail }) => async (d
             : error?.response?.data?.message;
 
         dispatch({ type: CHATS_FAIL, payload: errorMessage });
+
+        // Call the onFail callback if provided
+        if (onFail) {
+            onFail(errorMessage);
+        }
+    }
+};
+
+
+
+
+
+export const userSearch = ({ keyword, onSuccess, onFail }) => async (dispatch) => {
+    try {
+        dispatch({ type: USER_CHATS_SEARCH_REQUEST });
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                'access-control-allow-credentials': true
+            },
+            withCredentials: true
+        };
+
+        const { data } = await buildClient().get(`${chatSearchUserUrl}/?search=${keyword}`, config);
+        console.log("data", data)
+        dispatch({ type: USER_CHATS_SEARCH_SUCCESS, payload: data.users });
+
+        // Call the onSuccess callback if provided
+        if (onSuccess) {
+            onSuccess(data);
+        }
+    } catch (error) {
+        console.log("error during chats", error);
+        const errorMessage = error?.response?.data?.errors?.length > 0
+            ? error?.response?.data?.errors[0]
+            : error?.response?.data?.message;
+
+        dispatch({ type: USER_CHATS_SEARCH_FAIL, payload: errorMessage });
 
         // Call the onFail callback if provided
         if (onFail) {
