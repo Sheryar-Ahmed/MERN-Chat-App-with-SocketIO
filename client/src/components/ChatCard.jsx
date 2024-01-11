3336c53e686ec8b6e08541d5c94b9f824132bdda
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-12 01:22:06 +0500

    selectedchat backgroun to green

--- client/src/components/ChatCard.jsx
+++ client/src/components/ChatCard.jsx
@@ -2,56 +2,58 @@ import React from 'react';
 import { Typography } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 
-const ChatCard = ({ username, Message, Time, id }) => {
+const ChatCard = ({ username, Message, Time, id, isSelected, onClick }) => {
     return (
-        <div key={id} className='w-full p-2 border flex flex-row justify-center items-center gap-3'>
-            {/* avatar */}
-            <div className='w-1/5'>
-                <Avatar
-                    sx={{ width: 56, height: 56 }}
-                    alt={username} src="/static/images/avatar/1.jpg"
-                />
-            </div>
-            {/* chats and time */}
-            <div className='w-4/5 flex flex-col'>
-                <div className='w-full flex flex-row justify-between items-center'>
-                    <Typography
-                        variant="h6"
-                        component="h6"
-                        sx={{
-                            width: '80%',
-                            whiteSpace: 'nowrap',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            fontSize: 18,
-                        }}
-                    >
-                        {username}
-                    </Typography>
-
-                    <Typography variant="h6" component="h6" style={{ width: '30%', fontSize: 14 }}>
-                        {Time}
-                    </Typography>
-
-                </div>
-                <div>
-                    <Typography
-                        variant="h6"
-                        component="h6"
-                        sx={{
-                            width: '100%',
-                            whiteSpace: 'nowrap',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            fontSize: 14,
-                        }}
-                    >
-                        {Message}
-                    </Typography>
-                </div>
-            </div>
+      <div
+        key={id}
+        onClick={onClick}
+        className={`w-full p-2 border flex flex-row justify-center items-center gap-3 ${
+          isSelected ? 'bg-green-200' : ''
+        }`}
+      >
+        {/* avatar */}
+        <div className='w-1/5'>
+          <Avatar sx={{ width: 56, height: 56 }} alt={username} src='/static/images/avatar/1.jpg' />
         </div>
-    )
-}
-
-export default ChatCard;
+        {/* chats and time */}
+        <div className='w-4/5 flex flex-col'>
+          <div className='w-full flex flex-row justify-between items-center'>
+            <Typography
+              variant='h6'
+              component='h6'
+              sx={{
+                width: '80%',
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                fontSize: 18,
+              }}
+            >
+              {username}
+            </Typography>
+  
+            <Typography variant='h6' component='h6' style={{ width: '30%', fontSize: 14 }}>
+              {Time}
+            </Typography>
+          </div>
+          <div>
+            <Typography
+              variant='h6'
+              component='h6'
+              sx={{
+                width: '100%',
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                fontSize: 14,
+              }}
+            >
+              {Message}
+            </Typography>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ChatCard;
--- client/src/components/ChatList.jsx
+++ client/src/components/ChatList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import { Typography } from '@mui/material';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
@@ -6,137 +6,67 @@ import ChatCard from './ChatCard';
 import { userChatList } from '../state/actions/chatActions';
 import { useDispatch, useSelector } from 'react-redux';
 
-
-
 const ChatList = () => {
   const dispatch = useDispatch();
-
+  const [selectedChatId, setSelectedChatId] = useState(null);
 
   useEffect(() => {
-    dispatch(userChatList({
-      onSuccess: (data) => {
-        // Handle success logic, e.g., redirect or any other action
-        console.log(data);
-      },
-      onFail: (errorMessage) => {
-        // Handle failure logic, e.g., show an error message
-        toast.error(errorMessage, {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
-      }
-    }));
-  }, [dispatch])
-
-
-
-
-
-
+    dispatch(
+      userChatList({
+        onSuccess: (data) => {
+          // Handle success logic, e.g., redirect or any other action
+          console.log(data);
+        },
+        onFail: (errorMessage) => {
+          // Handle failure logic, e.g., show an error message
+          toast.error(errorMessage, {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+          });
+        },
+      })
+    );
+  }, [dispatch]);
 
   const { loading, chats } = useSelector((state) => state.chats);
-  console.log("cha", chats);
+  console.log('cha', chats);
+
+  const handleChatCardClick = (chatId) => {
+    setSelectedChatId(chatId === selectedChatId ? null : chatId);
+  };
 
   return (
     <div className='min-w-[320px] w-1/4 border-r p-4 h-full'>
       {/* header */}
       <div className='w-full flex flex-row justify-between algin-center'>
-        <Typography variant="h6" component="h6">
+        <Typography variant='h6' component='h6'>
           Chats
         </Typography>
-        <Button variant="outlined" endIcon={<AddCircleOutlineOutlinedIcon />}>
+        <Button variant='outlined' endIcon={<AddCircleOutlineOutlinedIcon />}>
           Delete
         </Button>
       </div>
       {/* chatslist */}
       <div className='w-full mt-2 p-2 bg-[#ECEFF1] h-full max-h-full flex flex-col items-center justify-start gap-2 overflow-y-auto'>
-        {chats.map(({ id, Message, users, Time }) =>
-          <ChatCard id={id} Message={Message} username={users[1].username} Time={Time} />
-        )}
+        {chats.map(({ _id, Message, users, Time }) => (
+          <ChatCard
+            id={_id}
+            Message={Message}
+            username={users[1].username}
+            Time={Time}
+            isSelected={selectedChatId === _id}
+            onClick={() => handleChatCardClick(_id)}
+          />
+        ))}
       </div>
-
     </div>
   );
 };
-export default ChatList;
 
-const chatListDynamic = [
-  {
-    id: 1,
-    username: "Sheryar Ahmed",
-    Time: "12:19 PM",
-    Message: "I will go to New York, Japan or German only as I am a tech person.",
-  },
-  {
-    id: 2,
-    username: "Alice Johnson",
-    Time: "01:45 PM",
-    Message: "Exploring new opportunities and challenges!",
-  },
-  {
-    id: 3,
-    username: "Bob Smith",
-    Time: "03:22 PM",
-    Message: "Just finished a great coding session. Feeling productive!",
-  },
-  {
-    id: 4,
-    username: "Elena Rodriguez",
-    Time: "05:10 PM",
-    Message: "Working on a cool project. Can't wait to share it with you all!",
-  },
-  {
-    id: 5,
-    username: "David Lee",
-    Time: "07:55 PM",
-    Message: "Anyone up for a coding challenge?",
-  },
-  {
-    id: 6,
-    username: "Sophie Williams",
-    Time: "09:30 PM",
-    Message: "Learning new technologies is always exciting!",
-  },
-  {
-    id: 7,
-    username: "Michael Brown",
-    Time: "11:12 PM",
-    Message: "Late-night coding session. The best time for creativity!",
-  },
-  {
-    id: 8,
-    username: "Olivia Taylor",
-    Time: "01:02 AM",
-    Message: "Dreaming big and working hard to make it happen!",
-  },
-  {
-    id: 9,
-    username: "Daniel Kim",
-    Time: "03:41 AM",
-    Message: "Coding is my passion. Every line of code tells a story!",
-  },
-  {
-    id: 7,
-    username: "Michael Brown",
-    Time: "11:12 PM",
-    Message: "Late-night coding session. The best time for creativity!",
-  },
-  {
-    id: 8,
-    username: "Olivia Taylor",
-    Time: "01:02 AM",
-    Message: "Dreaming big and working hard to make it happen!",
-  },
-  {
-    id: 9,
-    username: "Daniel Kim",
-    Time: "03:41 AM",
-    Message: "Coding is my passion. Every line of code tells a story!",
-  },
-];
+export default ChatList;
