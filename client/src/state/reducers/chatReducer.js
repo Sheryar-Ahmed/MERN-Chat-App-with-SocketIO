4c479b36088e26fed0e3d2415cb96cd6014ebe9e
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-13 18:06:13 +0500

    created groupchat creation reducer

--- client/src/state/reducers/chatReducer.js
+++ client/src/state/reducers/chatReducer.js
@@ -1,4 +1,4 @@
-import { ACCESS_FAIL, ACCESS_REQUEST, ACCESS_SUCCESS, CHATS_FAIL, CHATS_REQUEST, CHATS_SUCCESS, USER_CHATS_SEARCH_FAIL, USER_CHATS_SEARCH_REQUEST, USER_CHATS_SEARCH_SUCCESS } from '../constants/chatConstant';
+import { ACCESS_FAIL, ACCESS_REQUEST, ACCESS_SUCCESS, CHATS_FAIL, CHATS_REQUEST, CHATS_SUCCESS, CREATE_GROUP_CHAT_FAIL, CREATE_GROUP_CHAT_REQUEST, CREATE_GROUP_CHAT_SUCCESS, USER_CHATS_SEARCH_FAIL, USER_CHATS_SEARCH_REQUEST, USER_CHATS_SEARCH_SUCCESS } from '../constants/chatConstant';
 
 export const chatsReducers = (state = { chats: [] }, action) => {
     switch (action.type) {
@@ -70,6 +70,35 @@ export const accessChatReducer = (state = { FullChat: [] }, action) => {
                 ...state,
                 loading: false,
                 FullChat: [],
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+
+
+
+export const createGroupChatReducer = (state = { groupChat: {} }, action) => {
+    switch (action.type) {
+        case CREATE_GROUP_CHAT_REQUEST:
+            return {
+                loading: true,
+                groupChat: {},
+            };
+        case CREATE_GROUP_CHAT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                groupChat: action.payload,
+                error: null,
+            };
+        case CREATE_GROUP_CHAT_FAIL:
+            return {
+                ...state,
+                loading: false,
+                groupChat: {},
                 error: action.payload
             };
         default:
