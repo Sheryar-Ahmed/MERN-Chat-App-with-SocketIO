23fa3ab54b09a963e7c4aa94ca97c16da07e3f53
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-13 18:07:46 +0500

    integrated groupchat creation reducer to store.

--- client/src/store.js
+++ client/src/store.js
@@ -2,13 +2,14 @@ import { combineReducers, createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { userReducer } from "./state/reducers/userReducer";
-import { accessChatReducer, chatUsersSearch, chatsReducers } from "./state/reducers/chatReducer";
+import { accessChatReducer, chatUsersSearch, chatsReducers, createGroupChatReducer } from "./state/reducers/chatReducer";
 
 const reducer = combineReducers({
     user: userReducer,
     chats: chatsReducers,
     searchUsers: chatUsersSearch,
     accessChat: accessChatReducer,
+    creategroupChat: createGroupChatReducer
 });
 
 
