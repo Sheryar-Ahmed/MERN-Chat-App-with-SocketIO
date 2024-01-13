c6a041986a581aa5a03546e30641a52bd775d1cc
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-13 18:05:54 +0500

    creategroupchaturl is added

--- client/src/utils/requestUrls.jsx
+++ client/src/utils/requestUrls.jsx
@@ -8,6 +8,7 @@ export const registerURL = `/api/users/register`;
 export const chatUrl = `/api/chat/all`;
 export const chatSearchUserUrl = `/api/chat/users`;
 export const accessChatUrl = `/api/chat`;
+export const createGroupChatURL = `/api/chat/group`;
 
 const buildClient = () => {
     return axios.create({
