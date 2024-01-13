8eed8c66512bd6581ffca1ff32755091ac73d55d
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-13 15:36:43 +0500

    added chataccessreducer to store

--- client/src/store.js
+++ client/src/store.js
@@ -2,12 +2,13 @@ import { combineReducers, createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { userReducer } from "./state/reducers/userReducer";
-import { chatUsersSearch, chatsReducers } from "./state/reducers/chatReducer";
+import { accessChatReducer, chatUsersSearch, chatsReducers } from "./state/reducers/chatReducer";
 
 const reducer = combineReducers({
     user: userReducer,
     chats: chatsReducers,
-    searchUsers: chatUsersSearch
+    searchUsers: chatUsersSearch,
+    accessChat: accessChatReducer,
 });
 
 
