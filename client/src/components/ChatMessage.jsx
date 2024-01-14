e10ad525887c1ce233cbe36ab5c4f286b09c6a93
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-14 23:04:50 +0500

    show modal of user for chat and modal of group for group chat on click

--- client/src/components/ChatMessage.jsx
+++ client/src/components/ChatMessage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import UserInfoModal from './UserInfor';
+import GroupModalUpdate from './GroupUpdateModal';
+import { useSelector } from 'react-redux';
 
 const ChatMessage = () => {
 
   const [userInfoOpen, setUserInfoOpen] = React.useState(false);
   const handleOpenUserInfo = () => setUserInfoOpen(true);
 
+  const { FullChat } = useSelector((state) => state.accessChat);
   return (
     <div className='flex-1 p-4'>
       {/* information header */}
@@ -20,7 +23,11 @@ const ChatMessage = () => {
           <RemoveRedEyeOutlinedIcon
             onClick={handleOpenUserInfo}
           />
-          <UserInfoModal open={userInfoOpen} setOpen={setUserInfoOpen} />
+          {FullChat && FullChat.isGroupChat
+            ?
+            <GroupModalUpdate open={userInfoOpen} setOpen={setUserInfoOpen} />
+            :
+            <UserInfoModal open={userInfoOpen} setOpen={setUserInfoOpen} user={FullChat.users[1]} />}
         </div>
       </div>
       {/* chat messages */}
