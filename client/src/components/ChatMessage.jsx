faaeda50f0f837178a17688f755af21ee969bf89
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-15 01:46:51 +0500

    created a chatmessage box

--- client/src/components/ChatMessage.jsx
+++ client/src/components/ChatMessage.jsx
@@ -9,25 +9,32 @@ const ChatMessage = () => {
   const [userInfoOpen, setUserInfoOpen] = React.useState(false);
   const handleOpenUserInfo = () => setUserInfoOpen(true);
 
-  const { FullChat } = useSelector((state) => state.accessChat);
+  const { selectedChat } = useSelector((state) => state.selectedChat);
+
   return (
     <div className='flex-1 p-4'>
       {/* information header */}
       <div className='w-full h-10 bg-[#f5f5f5] flex flex-row items-center justify-between p-2'>
         <div>
           {/* chatname */}
-          <span>Demo of Youtube</span>
+          <span>{selectedChat && selectedChat.isGroupChat ? selectedChat.chatName : selectedChat.users[1].username}</span>
         </div>
         <div>
           {/* information user */}
           <RemoveRedEyeOutlinedIcon
             onClick={handleOpenUserInfo}
           />
-          {FullChat && FullChat.isGroupChat
-            ?
-            <GroupModalUpdate open={userInfoOpen} setOpen={setUserInfoOpen} />
-            :
-            <UserInfoModal open={userInfoOpen} setOpen={setUserInfoOpen} user={FullChat.users[1]} />}
+          {!selectedChat ? (
+            <span>I am not available</span>
+          ) : selectedChat.isGroupChat ? (
+            <GroupModalUpdate open={userInfoOpen} setOpen={setUserInfoOpen} groupInfo={selectedChat} />
+          ) : (
+            selectedChat.users && selectedChat.users[1] ? (
+              <UserInfoModal open={userInfoOpen} setOpen={setUserInfoOpen} user={selectedChat.users[1]} />
+            ) : (
+              <span>No user information available</span>
+            )
+          )}
         </div>
       </div>
       {/* chat messages */}
