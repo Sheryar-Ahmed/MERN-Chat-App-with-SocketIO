d16e750d2eabe452aff3849acbce0c04f73fa368
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-14 22:19:25 +0500

    user details reducer is integrated to the user reducer same as login and register.

--- client/src/state/reducers/userReducer.js
+++ client/src/state/reducers/userReducer.js
@@ -4,19 +4,25 @@ import {
     LOGIN_FAIL,
     REGISTER_REQUEST,
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_SUCCESS
 } from '../constants/userConstant';
 
 export const userReducer = (state = { user: {} }, action) => {
     switch (action.type) {
         case LOGIN_REQUEST:
         case REGISTER_REQUEST:
+        case USER_DETAILS_REQUEST:
+
             return {
                 loading: true,
                 isAuthenticated: false,
             };
         case LOGIN_SUCCESS:
         case REGISTER_SUCCESS:
+        case USER_DETAILS_SUCCESS:
             return {
                 ...state,
                 loading: false,
@@ -25,6 +31,7 @@ export const userReducer = (state = { user: {} }, action) => {
             };
         case LOGIN_FAIL:
         case REGISTER_FAIL:
+        case USER_DETAILS_FAIL:
             return {
                 ...state,
                 loading: false,
