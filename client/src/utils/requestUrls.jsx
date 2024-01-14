5b0a1d6880fe5d458460d15da767029a7e983dd7
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-14 22:19:01 +0500

    uesr details fetch url is defined.

--- client/src/utils/requestUrls.jsx
+++ client/src/utils/requestUrls.jsx
@@ -5,6 +5,8 @@ const HOST = 'http://localhost:3000';
 
 export const loginURL = `/api/users/login`;
 export const registerURL = `/api/users/register`;
+export const userDetailsURL = `/api/users/me`;
+
 export const chatUrl = `/api/chat/all`;
 export const chatSearchUserUrl = `/api/chat/users`;
 export const accessChatUrl = `/api/chat`;
