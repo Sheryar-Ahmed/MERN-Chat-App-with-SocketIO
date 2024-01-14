139c0d52b6ecd3f94698ddbbbe696a09fc03e4fc
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-15 01:42:34 +0500

    defined rename, add members and remove member from group with only admin.

--- client/src/utils/requestUrls.jsx
+++ client/src/utils/requestUrls.jsx
@@ -11,6 +11,9 @@ export const chatUrl = `/api/chat/all`;
 export const chatSearchUserUrl = `/api/chat/users`;
 export const accessChatUrl = `/api/chat`;
 export const createGroupChatURL = `/api/chat/group`;
+export const renameGroupURL = `/api/chat/group/rename`;
+export const addMembersToGroupURL = `/api/chat/group/add`;
+export const removeFromGroupURL = `/api/chat/group/remove`;
 
 const buildClient = () => {
     return axios.create({
