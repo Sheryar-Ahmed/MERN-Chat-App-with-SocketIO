7adb06c7d5c0e238506a14465fc890fe6e0e5155
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-16 21:48:29 +0500

    get all messages action is done.

--- client/src/state/actions/messageActions.js
+++ client/src/state/actions/messageActions.js
@@ -1,5 +1,5 @@
-import buildClient, { sendMessageURL } from '../../utils/requestUrls';
-import { SEND_MESSAGE_FAIL, SEND_MESSAGE_REQUEST, SEND_MESSAGE_SUCCESS } from '../constants/messageConstants';
+import buildClient, { getAllMessagesURL, sendMessageURL } from '../../utils/requestUrls';
+import { GET_ALL_MESSAGES_REQUEST, GET_ALL_MESSAGES_SUCCESS, SEND_MESSAGE_FAIL, SEND_MESSAGE_REQUEST, SEND_MESSAGE_SUCCESS } from '../constants/messageConstants';
 
 
 export const sendMessageAction = ({ chatId, content, onFail, onSuccess }) => async (dispatch) => {
@@ -23,6 +23,41 @@ export const sendMessageAction = ({ chatId, content, onFail, onSuccess }) => asy
         }
     } catch (error) {
         console.log("error during sending message", error);
+        const errorMessage = error?.response?.data?.errors?.length > 0
+            ? error?.response?.data?.errors[0]
+            : error?.response?.data?.message;
+
+        dispatch({ type: SEND_MESSAGE_FAIL, payload: errorMessage });
+
+        // Call the onFail callback if provided
+        if (onFail) {
+            onFail(errorMessage);
+        }
+    }
+};
+
+
+export const getAllMessagesAction = ({ chatId, onFail, onSuccess }) => async (dispatch) => {
+    try {
+        dispatch({ type: GET_ALL_MESSAGES_REQUEST });
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                'access-control-allow-credentials': true
+            },
+            withCredentials: true
+        };
+
+        const { data } = await buildClient().get(`${getAllMessagesURL}/${chatId}`, config);
+        dispatch({ type: GET_ALL_MESSAGES_SUCCESS, payload: data.messages });
+
+        // Call the onSuccess callback if provided
+        if (onSuccess) {
+            onSuccess(data);
+        }
+    } catch (error) {
+        console.log("error during getting all messages", error);
         const errorMessage = error?.response?.data?.errors?.length > 0
             ? error?.response?.data?.errors[0]
             : error?.response?.data?.message;
