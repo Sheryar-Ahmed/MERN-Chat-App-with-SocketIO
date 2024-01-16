c8a42365624d447666f3f2f2a8a337fd7f993688
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-16 22:18:39 +0500

    chatmessages of a user are displayed in a beautiful way.

--- client/src/components/ChatMessage.jsx
+++ client/src/components/ChatMessage.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import UserInfoModal from './UserInfor';
 import GroupModalUpdate from './GroupUpdateModal';
 import { useSelector } from 'react-redux';
 
 const ChatMessage = () => {
-
   const [userInfoOpen, setUserInfoOpen] = React.useState(false);
   const handleOpenUserInfo = () => setUserInfoOpen(true);
 
   const { selectedChat } = useSelector((state) => state.selectedChat);
+  const { messages } = useSelector((state) => state.allMessages);
+  const { user } = useSelector((state) => state.user);
 
   return (
     <div className='flex-1 p-4'>
@@ -17,7 +18,7 @@ const ChatMessage = () => {
       <div className='w-full h-10 bg-[#f5f5f5] flex flex-row items-center justify-between p-2'>
         <div>
           {/* chatname */}
-          <span>{selectedChat && selectedChat.isGroupChat ? selectedChat.chatName :             selectedChat.users && selectedChat.users[1] && selectedChat.users[1].username}</span>
+          <span>{selectedChat && selectedChat.isGroupChat ? selectedChat.chatName : selectedChat.users && selectedChat.users[1] && selectedChat.users[1].username}</span>
         </div>
         <div>
           {/* information user */}
@@ -37,9 +38,29 @@ const ChatMessage = () => {
           )}
         </div>
       </div>
-      {/* chat messages */}
-      <div>
-
+      <div className='w-full flex flex-col items-start justify-center'>
+        {/* chat messages */}
+        <div className='w-full h-full flex flex-col items-start justify-start gap-2 p-2'>
+          {messages?.map(
+            (item) => <div
+              key={item._id}
+              className={`w-full flex flex-row
+              ${user.id === item.sender.id
+                  ?
+                  'items-center justify-end'
+                  :
+                  'items-start'}`}
+            >
+              <span className={`p-2 rounded ${user.id === item.sender.id
+                ?
+                'bg-[#bbdefb]'
+                :
+                'bg-[#b2ebf2]'}`}>
+                {item?.content}
+              </span>
+            </div>
+          )}
+        </div>
         {/* input messages user */}
         <div>
 
