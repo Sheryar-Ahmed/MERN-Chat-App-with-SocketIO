c2f059937c356756d6c42bd64fc95d3c4a67e81b
sheryarahmedse <royalsheryar505@gmail.com>
2024-01-16 21:46:18 +0500

    get all messages reducer is defined.

--- client/src/state/reducers/messageReducer.js
+++ client/src/state/reducers/messageReducer.js
@@ -1,5 +1,5 @@
 
-import { SEND_MESSAGE_FAIL, SEND_MESSAGE_REQUEST, SEND_MESSAGE_SUCCESS } from '../constants/messageConstants';
+import { GET_ALL_MESSAGES_FAIL, GET_ALL_MESSAGES_REQUEST, GET_ALL_MESSAGES_SUCCESS, SEND_MESSAGE_FAIL, SEND_MESSAGE_REQUEST, SEND_MESSAGE_SUCCESS } from '../constants/messageConstants';
 
 export const messageSendReducer = (state = { message: {} }, action) => {
     switch (action.type) {
@@ -20,6 +20,32 @@ export const messageSendReducer = (state = { message: {} }, action) => {
                 ...state,
                 loading: false,
                 message: {},
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+export const getAllMessagesReducer = (state = { messages: [] }, action) => {
+    switch (action.type) {
+        case GET_ALL_MESSAGES_REQUEST:
+            return {
+                loading: true,
+                messages: [],
+            };
+        case GET_ALL_MESSAGES_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                messages: action.payload,
+                error: null,
+            };
+        case GET_ALL_MESSAGES_FAIL:
+            return {
+                ...state,
+                loading: false,
+                messages: [],
                 error: action.payload
             };
         default:
